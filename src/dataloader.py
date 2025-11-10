527c74081f62cdacbcaed48ee5a0776fcbb891ba
Thomas <twing@hmc.edu>
2025-11-09 20:59:52 -0800

    Built a basic foundation for the project to be built off of!

---
+++ src/dataloader.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+from functools import wraps
+
+class Trial:
+    def __init__(self, video_path):
+        if isinstance(video_path, str):
+            self.frames = self.load_mov_as_array(video_path)
+        elif isinstance(video_path, list):
+            self.frames = video_path
+            if video_path[0].ndim == 3:
+                self.is_gray = False
+            else:
+                self.is_gray = True
+        else:
+            TypeError("Not a valid type")
+        
+
+        # self.frames = self.load_mov_as_array(video_path)
+        self.is_gray = False
+    
+
+    def load_mov_as_array(self, video_path):
+        cap = cv2.VideoCapture(video_path)
+        if not cap.isOpened():
+            print("Error: Could not open video file.")
+        frames = []
+        while True:
+            ret, frame = cap.read() # Reads a frame
+            if not ret:
+                break
+            frames.append(frame)
+        cap.release()
+        return frames
+    
+
+    def play_video(self):
+        for frame in self.frames:
+            cv2.imshow("Video Frame", frame)
+
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+        
+        cv2.destroyAllWindows()
+    
+    
+    @staticmethod
+    def for_all_frames(func):
+        @wraps(func)
+        def wrapper(self, inplace=False, *args, **kwargs):
+            out_frames = [func(self, frame, *args, **kwargs) for frame in self.frames]
+
+            if inplace:
+                self.frames = out_frames
+                return self
+            
+            return Trial(out_frames)
+        
+        return wrapper
+    
+    
+    @for_all_frames
+    def to_gray(self, frame):
+        if frame.ndim == 2:
+            return frame
+        self.is_gray = True
+        return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+
+    @for_all_frames
+    def gauss_blur(self, frame, ksize=(3,3), sigmaX=3):
+        return cv2.GaussianBlur(frame, ksize, sigmaX)
+    
+
+    
+
+    def __getitem__(self, index):
+        return self.frames[index]
+
+
+    
